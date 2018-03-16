2d6fafaf1c32bc8ee126aa9ecb44910fcb033916
Alan Davis <alan.davis@alfresco.com>
2018-03-16 07:29:20 +0000

    REPO-3264 Fix startup of transformers

--- alfresco-docker-imagemagick/src/main/java/org/alfresco/transformer/Application.java
+++ alfresco-docker-imagemagick/src/main/java/org/alfresco/transformer/Application.java
@@ -27,8 +27,11 @@ package org.alfresco.transformer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application
 {
     public static void main(String[] args)
