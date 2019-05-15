4817d081228269d9d41352dc2797ce080b01fcc9
Cezar.Leahu <cezar.leahu@ness.com>
2019-05-15 16:40:54 +0300

    ATS-400 : Cache LibreOffice distribution

--- _ci/init.sh
+++ _ci/init.sh
@@ -9,6 +9,15 @@ mkdir -p ${HOME}/.m2 && cp -rf _ci/settings.xml ${HOME}/.m2/
 echo "${QUAY_PASSWORD}" | docker login -u="alfresco+bamboo" --password-stdin quay.io
 find "${HOME}/.m2/repository/" -type d -name "*-SNAPSHOT*" | xargs -r -l rm -rf
 
+# Cache the LibreOffice distribution, as it is takes a long time to download and it can cause the
+# build to fail (no output for more than 10 minutes)
+LIBREOFFICE_RPM_URL="https://nexus.alfresco.com/nexus/service/local/repositories/thirdparty/content/org/libreoffice/libreoffice-dist/5.4.6/libreoffice-dist-5.4.6-linux.gz"
+if [ ! -f "${HOME}/.m2/repository/libreoffice-dist-5.4.6-linux.gz" ]; then
+    travis_wait curl -s -S ${LIBREOFFICE_RPM_URL} -o "${HOME}/.m2/repository/libreoffice-dist-5.4.6-linux.gz"
+fi
+cp "${HOME}/.m2/repository/libreoffice-dist-5.4.6-linux.gz" alfresco-docker-libreoffice/
+
+
 popd
 set +vex
 echo "=========================== Finishing Init Script =========================="
