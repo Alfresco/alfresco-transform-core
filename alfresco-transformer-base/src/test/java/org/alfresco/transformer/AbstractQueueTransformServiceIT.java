70652dcb3196e92f01657f11011c06d6bc76bcd7
CezarLeahu <35226487+CezarLeahu@users.noreply.github.com>
2019-05-20 14:06:25 +0300

    ATS-400 : Update Copyright (#23)
    * ATS-400 : Update Copyright
    
    * ATS-400 : Update maven license plugin configuration
    
    * ATS-400 : Update Copyright with the license-maven-plugin
    
    * ATS-400 : Update licence-maven-plugin default configuration

--- alfresco-transformer-base/src/test/java/org/alfresco/transformer/AbstractQueueTransformServiceIT.java
+++ alfresco-transformer-base/src/test/java/org/alfresco/transformer/AbstractQueueTransformServiceIT.java
@@ -1,12 +1,22 @@
 /*
  * #%L
- * Alfresco Enterprise Repository
+ * Alfresco Transform Core
  * %%
- * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * Copyright (C) 2005 - 2019 Alfresco Software Limited
  * %%
- * License rights for this program may be obtained from Alfresco Software, Ltd.
- * pursuant to a written agreement and any use of this program without such an
- * agreement is prohibited.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
 package org.alfresco.transformer;
