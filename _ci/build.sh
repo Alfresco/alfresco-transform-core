01c160fe2500515db74d6a229c100402de9480db
Cezar.Leahu <cezar.leahu@ness.com>
2019-08-21 09:48:02 +0300

    ATS-534 : Security - Vulnerability in Quartz - CVE-2019-13990
    - publish SNAPSHOT artifacts from the master branch
    - exclude quartz dependency from tika-parsers

--- _ci/build.sh
+++ _ci/build.sh
@@ -8,8 +8,11 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 # Always build the image, but only publish from the "master" branch
 [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && PROFILE="internal" || PROFILE="local"
 
+# If the branch is "master" and the commit is not a Pull Request then deploy the JAR SNAPSHOT artifacts
+[ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && DEPLOY="deploy" || DEPLOY="install"
+
 mvn -B -U \
-    clean install \
+    clean ${DEPLOY} \
     -DadditionalOption=-Xdoclint:none -Dmaven.javadoc.skip=true \
     "-P${PROFILE},docker-it-setup"
 
