1b090b3d03d769a4eb18ae17ebfdb08ade2cdfca
Cezar.Leahu <cezar.leahu@ness.com>
2019-08-23 15:47:32 +0300

    Integration test for Alfresco PDF Renderer T-Engine

---
+++ alfresco-docker-alfresco-pdf-renderer/src/test/java/org/alfresco/transformer/TransformationIT.java
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * Alfresco Transform Core
+ * %%
+ * Copyright (C) 2005 - 2019 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * -
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ * -
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * -
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.transformer;
+
+import static java.nio.file.Files.readAllBytes;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Cezar Leahu
+ */
+public class TransformationIT
+{
+    private static final Logger logger = LoggerFactory.getLogger(TransformationIT.class);
+    private static final String ENGINE_URL = "http://localhost:8090";
+
+    @Test
+    public void testPdfToPng() throws Exception
+    {
+        sendTRequest2(ResourceUtils.getFile("classpath:quick.pdf"), "png");
+    }
+
+    private static void sendTRequest2(final File sourceFile, final String targetExtension) throws Exception
+    {
+        final RestTemplate restTemplate = new RestTemplate();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MULTIPART_FORM_DATA);
+        //headers.setAccept(MULTIPART_FORM_DATA_VALUE);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new ByteArrayResource(readAllBytes(sourceFile.toPath())));
+//        body.add("targetExtension", targetExtension);
+
+        final HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
+
+//        final ResponseEntity<Resource> response = restTemplate.exchange(ENGINE_URL + "/transform",
+//            POST, entity, Resource.class);
+
+        final ResponseEntity<Resource> response = restTemplate.postForEntity(ENGINE_URL + "/transform",
+            entity, Resource.class);
+
+        logger.info("Response: {}", response);
+    }
+}
