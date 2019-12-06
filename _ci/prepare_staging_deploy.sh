df3319064b8b9a6272ea1e5de393bcffaceefca2
Cezar.Leahu <cezar.leahu@ness.com>
2019-12-06 14:39:18 +0200

    ATS-628: Update build config on support branches

---
+++ _ci/prepare_staging_deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "========================== Starting Prepare Staging Deploy Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+# Identify latest annotated tag (latest version)
+export VERSION=$(git describe --abbrev=0 --tags)
+
+mkdir -p deploy_dir
+
+# Download the WhiteSource report
+mvn org.alfresco:whitesource-downloader-plugin:inventoryReport \
+    -N \
+    "-Dorg.whitesource.product=Transform Service" \
+    -DsaveReportAs=deploy_dir/3rd-party.xlsx
+
+echo "Local deploy directory content:"
+ls -lA deploy_dir
+
+popd
+set +vex
+echo "========================== Finishing Prepare Staging Deploy Script =========================="
