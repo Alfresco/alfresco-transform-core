e5f94d2e28b645407a006ad02ca5ba4457dd39ad
David Edwards <david.edwards@alfresco.com>
2020-04-16 13:45:14 +0100

    ATS-724 prevent duplicated deployments of repeated projects alfresco-transform-core and alfresco-transformer-base

--- _ci/test.sh
+++ _ci/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "=========================== Starting Build&Test Script ==========================="
+echo "=========================== Starting Test&Deploy Script ==========================="
 PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
@@ -12,16 +12,16 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && DEPLOY="deploy" || DEPLOY="test"
 
 # If the branch is "master" and the profile provided is not "aio-test" do not deploy alfresco-transformer-base
-[ "${1}" != "aio-test" ] && [ "${TRAVIS_BRANCH}" = "master" ] && IGNORE_BASE="-pl !alfresco-transformer-base" || IGNORE_BASE=""
+[ "${1}" != "aio-test" ] && [ "${TRAVIS_BRANCH}" = "master" ] && SKIP_DUP_DEPLOY="-Dparent.deploy.skip=true -Dtransformer.base.deploy.skip=true" || SKIP_DUP_DEPLOY=""
 
 mvn -B -U \
     clean ${DEPLOY} \
     -DadditionalOption=-Xdoclint:none -Dmaven.javadoc.skip=true \
-    "-P${PROFILE},docker-it-setup,${1}" ${IGNORE_BASE}
+    "-P${PROFILE},docker-it-setup,${1}" ${SKIP_DUP_DEPLOY}
 
 docker ps -a -q | xargs -r -l docker stop ; docker ps -a -q | xargs -r -l docker rm
 
 popd
 set +vex
-echo "=========================== Finishing Build&Test Script =========================="
+echo "=========================== Finishing Test&Deploy Script =========================="
 
