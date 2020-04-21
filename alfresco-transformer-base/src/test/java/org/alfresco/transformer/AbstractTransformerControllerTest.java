177077d070a6913fc3086a72418fcb2b0628cc1b
David Edwards <david.edwards@alfresco.com>
2020-04-21 13:02:56 +0100

    ATS-702 Fix PR

--- alfresco-transformer-base/src/test/java/org/alfresco/transformer/AbstractTransformerControllerTest.java
+++ alfresco-transformer-base/src/test/java/org/alfresco/transformer/AbstractTransformerControllerTest.java
@@ -49,7 +49,6 @@ import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
