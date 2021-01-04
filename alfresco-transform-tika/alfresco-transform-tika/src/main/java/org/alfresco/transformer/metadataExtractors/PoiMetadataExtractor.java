b8e3f878d74ff11fd82840f94f16c4d3d3cf9adf
alandavis <alan.davis@alfresco.com>
2021-01-04 16:28:59 +0000

    REPO-5191 Bug: T-Engine should provide mapping rather than the repo.
    Bug found while reviewing documents on how to create a custom metadata extractor. The original refactor had left the repo doing the mapping. It should have been passing the fully qualified repo properties to the T-Engine to do the mapping.

--- alfresco-transform-tika/alfresco-transform-tika/src/main/java/org/alfresco/transformer/metadataExtractors/PoiMetadataExtractor.java
+++ alfresco-transform-tika/alfresco-transform-tika/src/main/java/org/alfresco/transformer/metadataExtractors/PoiMetadataExtractor.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Transform Core
  * %%
- * Copyright (C) 2005 - 2020 Alfresco Software Limited
+ * Copyright (C) 2005 - 2021 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * -
@@ -26,10 +26,26 @@
  */
 package org.alfresco.transformer.metadataExtractors;
 
+import org.apache.poi.ooxml.POIXMLProperties;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.tika.embedder.Embedder;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.microsoft.ooxml.OOXMLParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.Set;
+import java.util.StringJoiner;
 
 /**
  * POI-based metadata extractor for Office 07 documents. See http://poi.apache.org/ for information on POI.
@@ -46,6 +62,34 @@ import org.slf4j.LoggerFactory;
  *
  * Uses Apache Tika
  *
+ * Also includes a sample POI metadata embedder to demonstrate it is possible to add custom T-Engines that will add
+ * metadata. This is not production code so no supported mimetypes exist in the {@code tika_engine_config.json}.
+ * Adding the following would make it available:
+ *
+ * <pre>
+ * {
+ *   "transformOptions": {
+ *     ...
+ *     "metadataEmbedOptions": [
+ *       {"value": {"name": "metadata", "required": true}}
+ *     ]
+ *   },
+ *   "transformers": [
+ *     ...
+ *     {
+ *       "transformerName": "SamplePoiMetadataEmbedder",
+ *       "supportedSourceAndTargetList": [
+ *         ...
+ *         {"sourceMediaType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "targetMediaType": "alfresco-metadata-embed"}
+ *       ],
+ *       "transformOptions": [
+ *         "metadataEmbedOptions"
+ *       ]
+ *     }
+ *   ]
+ * }
+ * </pre>
+
  * @author Nick Burch
  * @author Neil McErlean
  * @author Dmitry Velichkevich
@@ -64,5 +108,71 @@ public class PoiMetadataExtractor extends AbstractTikaMetadataExtractor
     protected Parser getParser()
     {
         return new OOXMLParser();
+    }
+
+    @Override
+    protected Embedder getEmbedder()
+    {
+        return new SamplePoiEmbedder();
+    }
+
+    private static class SamplePoiEmbedder implements Embedder
+    {
+        private static final Set<MediaType> SUPPORTED_EMBED_TYPES =
+                Collections.singleton(MediaType.application("vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+
+        @Override
+        public Set<MediaType> getSupportedEmbedTypes(ParseContext parseContext)
+        {
+            return SUPPORTED_EMBED_TYPES;
+        }
+
+        @Override
+        public void embed(Metadata metadata, InputStream inputStream, OutputStream outputStream, ParseContext parseContext)
+                throws IOException
+        {
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            POIXMLProperties props = workbook.getProperties();
+
+            POIXMLProperties.CoreProperties coreProp = props.getCoreProperties();
+            POIXMLProperties.CustomProperties custProp = props.getCustomProperties();
+
+            for (String name : metadata.names())
+            {
+                metadata.isMultiValued("description");
+                String value = null;
+                if (metadata.isMultiValued(name))
+                {
+                    String[] values = metadata.getValues(name);
+                    StringJoiner sj = new StringJoiner(", ");
+                    for (String s : values)
+                    {
+                        sj.add(s);
+                    }
+                    value = sj.toString();
+                }
+                else
+                {
+                    value = metadata.get(name);
+                }
+                switch (name)
+                {
+                    case "author":
+                        coreProp.setCreator(value);
+                        break;
+                    case "title":
+                        coreProp.setTitle(value);
+                        break;
+                    case "description":
+                        coreProp.setDescription(value);
+                        break;
+                    // There are other core values but this is sample code, so we will assume it is a custom value.
+                    default:
+                        custProp.addProperty(name, value);
+                        break;
+                }
+            }
+            workbook.write(outputStream);
+        }
     }
 }
