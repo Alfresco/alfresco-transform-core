add3f4829a7d4bae790d379dc3e23cf3a785d073
Nebil Kisa <nebil.kisa@alfresco.com>
2021-02-02 04:42:06 +0000

    - Add Maven Central to top of repo search list - Disable Maven connection pool

--- _ci/test.sh
+++ _ci/test.sh
@@ -12,15 +12,14 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && DEPLOY="deploy" || DEPLOY="verify"
 
 # Do not deploy snapshots for alfresco-transform-core and alfresco-transformer-base
-mvn -B -U \
+mvn -B -U -Dmaven.wagon.http.pool=false \
     clean ${DEPLOY} \
     -DadditionalOption=-Xdoclint:none -Dmaven.javadoc.skip=true \
     -Dparent.core.deploy.skip=true -Dtransformer.base.deploy.skip=true \
-    "-P${PROFILE},docker-it-setup,${1}" 
+    "-P${PROFILE},docker-it-setup,${1}"
 
 docker ps -a -q | xargs -r -l docker stop ; docker ps -a -q | xargs -r -l docker rm
 
 popd
 set +vex
 echo "=========================== Finishing Test&Deploy Script =========================="
-
