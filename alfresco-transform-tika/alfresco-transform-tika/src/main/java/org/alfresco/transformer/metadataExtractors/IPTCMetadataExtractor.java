a413d16a14adb9509bb9f9c4c8aa431eb1927811
David Edwards <david.edwards@alfresco.com>
2021-05-05 16:44:20 +0100

    remove unused imports

--- alfresco-transform-tika/alfresco-transform-tika/src/main/java/org/alfresco/transformer/metadataExtractors/IPTCMetadataExtractor.java
+++ alfresco-transform-tika/alfresco-transform-tika/src/main/java/org/alfresco/transformer/metadataExtractors/IPTCMetadataExtractor.java
@@ -28,7 +28,6 @@ package org.alfresco.transformer.metadataExtractors;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
