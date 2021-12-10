6a69611d1397699e4e783ede6f53c01d171e55c1
Jamal Kaabi-Mofrad <jamal.kaabimofrad@alfresco.com>
2021-12-10 14:10:03 +0000

    ACS-2352: Added Veracode agent-based scanning integration.

--- _ci/source_clear.sh
+++ _ci/source_clear.sh
@@ -9,9 +9,13 @@ mvn -B -q clean install \
     -DskipTests \
     -Dmaven.javadoc.skip=true \
     com.srcclr:srcclr-maven-plugin:scan \
-    -Dcom.srcclr.apiToken=$SRCCLR_API_TOKEN > scan.log
+    -Dcom.srcclr.apiToken=${SRCCLR_API_TOKEN} > scan.log
 
 SUCCESS=$?   # this will read exit code of the previous command
+
+if [ -z "$VERACODE_FAILS_BUILD" ] || [ "$VERACODE_FAILS_BUILD" = false ] ; then
+    SUCCESS=0
+fi
 
 cat scan.log | grep -e 'Full Report Details' -e 'Failed'
 
