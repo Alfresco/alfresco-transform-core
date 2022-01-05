e967c10e949e84f92418a778dddb7e62227e1f6b
Jan Vonka <jan.vonka@alfresco.com>
2022-01-05 13:35:41 +0000

    ATS-935: Fix LibreOffice version in CI script
    - in three places (it looks like 3rd place was missed as part of ATS-935)

--- _ci/cache_artifacts.sh
+++ _ci/cache_artifacts.sh
@@ -5,7 +5,7 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-LIBREOFFICE_VERSION=6.3.5
+LIBREOFFICE_VERSION=7.0.6
 
 # Cache the LibreOffice distribution, as it is takes a long time to download and it can cause the
 # build to fail (no output for more than 10 minutes)
