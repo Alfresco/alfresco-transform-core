4335c425b77ca8930cca5411e30bebaf474c5956
alandavis <alan.davis@alfresco.com>
2022-01-27 15:56:31 +0000

    ACS-2479 AI Transformers do not start if built using ATS 1.5.0 components

--- alfresco-transformer-base/src/main/java/org/alfresco/transformer/config/WebApplicationConfig.java
+++ alfresco-transformer-base/src/main/java/org/alfresco/transformer/config/WebApplicationConfig.java
@@ -41,7 +41,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebApplicationConfig implements WebMvcConfigurer
 {
-
     @Override
     public void addInterceptors(InterceptorRegistry registry)
     {
