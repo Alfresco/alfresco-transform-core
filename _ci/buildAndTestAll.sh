fd00d714c2f26ab339dc25923249482db7a00307
alandavis <alan.davis@hyland.com>
2022-08-03 08:56:40 +0100

    Added buildAndTestAll.sh [skip ci]

--- _ci/buildAndTestAll.sh
+++ _ci/buildAndTestAll.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
+set -x
 
 mvn -U -Dmaven.wagon.http.pool=false clean install -DadditionalOption=-Xdoclint:none -Dmaven.javadoc.skip=true -Dparent.core.deploy.skip=true -Dtransformer.base.deploy.skip=true -Plocal,docker-it-setup
