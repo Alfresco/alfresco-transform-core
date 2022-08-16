2525b14b61cd952de45e579f6713a4ca44204f19
alandavis <alan.davis@hyland.com>
2022-08-16 17:27:37 +0100

    Fix header

--- model/src/main/java/org/alfresco/transform/registry/TransformerType.java
+++ model/src/main/java/org/alfresco/transform/registry/TransformerType.java
@@ -1,9 +1,23 @@
 /*
- *  Copyright 2015-2022 Alfresco Software, Ltd.  All rights reserved.
+ * #%L
+ * Alfresco Transform Model
+ * %%
+ * Copyright (C) 2015 - 2022 Alfresco Software Limited
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *  License rights for this program may be obtained from Alfresco Software, Ltd.
- *  pursuant to a written agreement and any use of this program without such an
- *  agreement is prohibited.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
  */
 package org.alfresco.transform.registry;
 
