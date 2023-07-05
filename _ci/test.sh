a346c0982efcc2c09c463242c84c5e4ea53e7035
Wojtek.Swieton <wojtek.swieton@hyland.com>
2023-07-05 16:26:56 +0200

    ACS-5039 - Revert test changes

--- _ci/test.sh
+++ _ci/test.sh
@@ -6,15 +6,17 @@ set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 # Always build the image, but only publish from the "master" branch
-[ "${PULL_REQUEST}" = "false" ] && [ "${BRANCH_NAME}" = "feature/ACS-5039_Transformers_multi-arch_support" ]
+[ "${PULL_REQUEST}" = "false" ] && [ "${BRANCH_NAME}" = "master" ] && PROFILE="internal" || PROFILE="local"
 
 # If the branch is "master" and the commit is not a Pull Request then deploy the JAR SNAPSHOT artifacts
-#[ "${PULL_REQUEST}" = "false" ] && [ "${BRANCH_NAME}" = "master" ] && DEPLOY="deploy" || DEPLOY="verify"
+[ "${PULL_REQUEST}" = "false" ] && [ "${BRANCH_NAME}" = "master" ] && DEPLOY="deploy" || DEPLOY="verify"
 
 # Do not deploy snapshots for alfresco-transform-core, alfresco-transformer-base and alfresco-base-t-engine
-
-
-mvn -B -U clean install -DskipTests -Pinternal -Ddocker.verbose=true
+mvn -B -U -Dmaven.wagon.http.pool=false \
+    clean ${DEPLOY} \
+    -DadditionalOption=-Xdoclint:none -Dmaven.javadoc.skip=true \
+    -Dparent.core.deploy.skip=true -Dtransformer.base.deploy.skip=true \
+    "-P${PROFILE},docker-it-setup,${1}"
 
 docker ps -a -q | xargs -r -l docker stop ; docker ps -a -q | xargs -r -l docker rm
 
