974f7a34825ad5ea1f39375d08efe649498f0ee2
mstrankowski <marcin.strankowski@hyland.com>
2023-09-19 15:21:19 +0200

    [skip tests] Updating copyright notices

--- engines/base/src/test/java/org/alfresco/transform/base/clients/SfsClient.java
+++ engines/base/src/test/java/org/alfresco/transform/base/clients/SfsClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2022 Alfresco Software, Ltd.  All rights reserved.
+ * Copyright 2015-2023 Alfresco Software, Ltd.  All rights reserved.
  *
  * License rights for this program may be obtained from Alfresco Software, Ltd.
  * pursuant to a written agreement and any use of this program without such an
