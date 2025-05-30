2811ab8ac6b5384147aa6a945213c19109fe5d45
KushalBanik <kushal.banik@hyland.com>
2025-05-30 17:16:50 +0530

    [MNT-25089] precommits fixed

--- engines/misc/src/main/java/org/alfresco/transform/misc/transformers/HtmlParserContentTransformer.java
+++ engines/misc/src/main/java/org/alfresco/transform/misc/transformers/HtmlParserContentTransformer.java
@@ -26,6 +26,9 @@
  */
 package org.alfresco.transform.misc.transformers;
 
+import static org.alfresco.transform.common.RequestParamMap.HTML_COLLAPSE;
+import static org.alfresco.transform.common.RequestParamMap.SOURCE_ENCODING;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -36,16 +39,15 @@ import java.nio.charset.Charset;
 import java.nio.charset.IllegalCharsetNameException;
 import java.util.Map;
 
-import org.alfresco.transform.base.TransformManager;
-import org.alfresco.transform.base.util.CustomTransformerFileAdaptor;
-import static org.alfresco.transform.common.RequestParamMap.HTML_COLLAPSE;
-import static org.alfresco.transform.common.RequestParamMap.SOURCE_ENCODING;
 import org.htmlparser.Parser;
 import org.htmlparser.beans.StringBean;
 import org.htmlparser.util.ParserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import org.alfresco.transform.base.TransformManager;
+import org.alfresco.transform.base.util.CustomTransformerFileAdaptor;
 
 /**
  * Content transformer which wraps the HTML Parser library for parsing HTML content.
