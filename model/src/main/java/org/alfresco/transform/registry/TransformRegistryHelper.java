9a46e161601691dad4936456032db8914202b64f
bsayan2 <sayan.bhattacharya@hyland.com>
2025-06-11 15:03:50 +0530

    Update TransformRegistryHelper.java license header

--- model/src/main/java/org/alfresco/transform/registry/TransformRegistryHelper.java
+++ model/src/main/java/org/alfresco/transform/registry/TransformRegistryHelper.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Transform Model
  * %%
- * Copyright (C) 2005 - 2022 Alfresco Software Limited
+ * Copyright (C) 2005 - 2025 Alfresco Software Limited
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
