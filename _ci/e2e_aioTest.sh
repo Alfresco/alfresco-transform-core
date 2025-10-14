0abac61b5d7b545f72b0daa22091cb49fda04255
Belal Ansari <belal.ansari@hyland.com>
2025-10-14 15:27:20 +0530

    ACS-10404 Test e2e AIO runtime compatibility with Java 21 and 25

---
+++ _ci/e2e_aioTest.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Test&Deploy Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+# Always build the image, but only publish from the "master" branch
+[ "${PULL_REQUEST}" = "false" ] && [ "${BRANCH_NAME}" = "master" ] && PROFILE="internal" || PROFILE="local"
+
+# If the branch is "master" and the commit is not a Pull Request then deploy the JAR SNAPSHOT artifacts
+[ "${PULL_REQUEST}" = "false" ] && [ "${BRANCH_NAME}" = "master" ] && DEPLOY="deploy" || DEPLOY="verify"
+
+# Do not deploy snapshots for alfresco-transform-core, alfresco-transformer-base and alfresco-base-t-engine
+mvn -B -U -Dmaven.wagon.http.pool=false \
+    clean ${DEPLOY} \
+    -Dtest='!AIOImageMagickTest,!AIOLibreOfficeTest,!AIOMiscTest,!AIOPdfRendererTest,!AIOTest,!AIOTikaTest' \
+    -DadditionalOption=-Xdoclint:none -Dmaven.javadoc.skip=true \
+    -Dparent.core.deploy.skip=true -Dtransformer.base.deploy.skip=true \
+    "-Dit.test=**/*IT.java" \
+    "-P${PROFILE},docker-it-setup,${1}"
+
+docker ps -a -q | xargs -r -l docker stop ; docker ps -a -q | xargs -r -l docker rm
+
+popd
+set +vex
+echo "=========================== Finishing Test&Deploy Script =========================="
