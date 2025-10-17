acb4a98832f6848328c9547b45bee7ef3623a5b6
Belal Ansari <belal.ansari@hyland.com>
2025-10-17 17:04:57 +0530

    ACS-10404 Test e2e AIO runtime compatibility with Java 21 and 25 (#1148)
    * ACS-10404 Test e2e AIO runtime compatibility with Java 21 and 25
    
    * ACS-10404 pull failure
    
    * ACS-10404 review changes
    
    * ACS-10404 review changes
    
    * ACS-10404 ADDITIONAL_MAVEN_OPTS
    
    * ACS-10404 bin/sh failure fix
    
    * ACS-10404 fix
    
    * ACS-10404 updating to latest java images

--- _ci/test.sh
+++ _ci/test.sh
@@ -11,12 +11,21 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 # If the branch is "master" and the commit is not a Pull Request then deploy the JAR SNAPSHOT artifacts
 [ "${PULL_REQUEST}" = "false" ] && [ "${BRANCH_NAME}" = "master" ] && DEPLOY="deploy" || DEPLOY="verify"
 
+if [ "${SMOKE_TESTS}" = "true" ]; then
+  ADDITIONAL_MAVEN_OPTS="-Dit.test=**/*IT.java"
+else
+  ADDITIONAL_MAVEN_OPTS=""
+fi
+
+source "$(dirname "${BASH_SOURCE[0]}")/java_base_image_map.sh"
+
 # Do not deploy snapshots for alfresco-transform-core, alfresco-transformer-base and alfresco-base-t-engine
 mvn -B -U -Dmaven.wagon.http.pool=false \
     clean ${DEPLOY} \
     -DadditionalOption=-Xdoclint:none -Dmaven.javadoc.skip=true \
     -Dparent.core.deploy.skip=true -Dtransformer.base.deploy.skip=true \
-    "-P${PROFILE},docker-it-setup,${1}"
+    "-P${PROFILE},docker-it-setup,${1}" \
+    ${ADDITIONAL_MAVEN_OPTS}
 
 docker ps -a -q | xargs -r -l docker stop ; docker ps -a -q | xargs -r -l docker rm
 
