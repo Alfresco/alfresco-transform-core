a91b4e273811d7c6466c29da64b4495a5b63ce19
Belal Ansari <belal.ansari@hyland.com>
2025-10-17 14:51:41 +0530

    ACS-10404 updating to latest java images

--- _ci/java_base_image_map.sh
+++ _ci/java_base_image_map.sh
@@ -2,8 +2,8 @@
 
 if [ -n "$JAVA_RUNTIME_VERSION" ]; then
   declare -A JAVA_IMAGE_MAP=(
-    [21]="alfresco/alfresco-base-java:jre21-rockylinux9@sha256:27297bffe7d45152194f08a8f38917a7e3d43b9a34dcfcdc29b83a80c4c33a10"
-    [25]="alfresco/alfresco-base-java:jre25-rockylinux9@sha256:7cc61edc5444e0eb69cfbf5d51716666ab8605fe9c8fdba75407e9484185fcd9"
+    [21]="alfresco/alfresco-base-java:jre21-rockylinux9@sha256:792079aa36a7a1076e7d48dd800b5de5ffc1cd48e8460fd7e56f7107d375c0cc"
+    [25]="alfresco/alfresco-base-java:jre25-rockylinux9@sha256:2e8664a6148157e0fd8c7861120c31768f510b5baaac86fd90c8b58aef5941eb"
   )
   JAVA_BASE_IMAGE="${JAVA_IMAGE_MAP[$JAVA_RUNTIME_VERSION]}"
   export JAVA_BASE_IMAGE
