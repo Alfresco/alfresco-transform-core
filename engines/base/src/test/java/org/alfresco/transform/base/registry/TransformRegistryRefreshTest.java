77ba012e3ee06281c087dfab5db32946fea4b8d7
vivekkr0311 <vivek.kumar@hyland.com>
2026-01-07 11:42:07 +0530

    mvn version modularization and license header update for the modified file

--- engines/base/src/test/java/org/alfresco/transform/base/registry/TransformRegistryRefreshTest.java
+++ engines/base/src/test/java/org/alfresco/transform/base/registry/TransformRegistryRefreshTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 Alfresco Software, Ltd.  All rights reserved.
+ * Copyright 2015-2026 Alfresco Software, Ltd.  All rights reserved.
  *
  * License rights for this program may be obtained from Alfresco Software, Ltd.
  * pursuant to a written agreement and any use of this program without such an
--- engines/imagemagick/src/test/java/org/alfresco/transform/imagemagick/ImageMagickTest.java
+++ engines/imagemagick/src/test/java/org/alfresco/transform/imagemagick/ImageMagickTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Transform Core
  * %%
- * Copyright (C) 2005 - 2025 Alfresco Software Limited
+ * Copyright (C) 2005 - 2026 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software.
  * -
